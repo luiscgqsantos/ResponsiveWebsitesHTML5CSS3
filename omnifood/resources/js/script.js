8ef0c520a7d2ef4efd1900e4ca5f50cce0689cad
Luís Santos <luis.cgq.santos@gmail.com>
2019-11-11 16:15:08 +0000

    Scrolling to elements

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    //Sticjy Navigation
     $('.js--section-features').waypoint(function(direction){
         if(direction == "down") {
             $('nav').addClass('sticky');
@@ -10,4 +11,32 @@ $(document).ready(function() {
         offset: '60px;'
     });
 
+    //Scroll on Buttons
+    $('.js--scroll-to-plan').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scrol-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+
+
+
+      
 });
