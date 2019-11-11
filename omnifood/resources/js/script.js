63370744d61224f4e11227604d942b8a57834897
Luís Santos <luis.cgq.santos@gmail.com>
2019-11-11 16:39:41 +0000

    Adding animations

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
     
-    /* Navigation scroll */
+    //Navigation scroll
     $(function() {
         $('a[href*=#]:not([href=#])').click(function() {
           if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -34,9 +34,33 @@ $(document).ready(function() {
             }
           }
         });
-      });
+    });
+    
+    
+    //Animations
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: "50%"
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: "50%"
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: "50%"
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: "50%"
+    });
 
 
-
-      
 });
